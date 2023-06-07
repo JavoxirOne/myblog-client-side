19624c504235ee69093e4a33e6eca975620c2601
Javohir <javoxirone@gmail.com>
2023-06-07 16:04:17 +0500

    fixed bugs

--- src/components/articleDetailPlaceholder/articleDetailPlaceholder.jsx
+++ src/components/articleDetailPlaceholder/articleDetailPlaceholder.jsx
@@ -1,48 +1,50 @@
 import "./articleDetailPlaceholder.css";
 const ArticleDetailPlaceholder = () => {
   return (
-    <div className="article-detail-placeholder">
-      <div className="article-detail-placeholder__img"></div>
-      <div className="article-detail-placeholder__content-line-title"></div>
+    <div className="container">
+      <div className="article-detail-placeholder">
+        <div className="article-detail-placeholder__img"></div>
+        <div className="article-detail-placeholder__content-line-title"></div>
         <div className="article-detail-placeholder__info">
-            <div className="article-detail-placeholder__info-item"></div>
-            <div className="article-detail-placeholder__info-item"></div>
-            <div className="article-detail-placeholder__info-item"></div>
+          <div className="article-detail-placeholder__info-item"></div>
+          <div className="article-detail-placeholder__info-item"></div>
+          <div className="article-detail-placeholder__info-item"></div>
         </div>
-      <div className="article-detail-placeholder__content">
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
-      </div>
-      <div className="article-detail-placeholder__content">
+        </div>
+        <div className="article-detail-placeholder__content">
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
           <div className="article-detail-placeholder__content-line"></div>
+        </div>
       </div>
     </div>
   );
